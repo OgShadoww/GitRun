11caf6d13abdbca36c8c638552240d022f9e0d20
Orest <galenzao@gmail.com>
2026-02-06 22:41:11 +0000

    Add feature for init the repo

--- gitrun.sh
+++ gitrun.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
 commit=$1
+flag1=$2
+name=$3
+
+git init
+
+if [ -n "$flag1" ] && [ -n "$name" ]; then
+  git add .
+  git commit -m "Initial commit"
+  git remote add origin https://github.com/OgShadoww/$name
+
+  echo "Repository initialized and remote set"
+  exit 0
+fi
 
 if [ -z "$commit" ]; then
   echo "Wrong parameter"
